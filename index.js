0988fadc4ebe118f68a996fd344dedeeb0f811ed
Mr. Rainbow <alexander.logsdon@stu.jefferson.kyschools.us>
2024-04-30 16:16:03 +0000

    e

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use('/', createProxyMiddleware({
     changeOrigin: true,
 }));
 
+app.use('/signup', express.static(path.join(__dirname, 'static', 'signup.html')))
+app.use('/login', express.static(path.join(__dirname, 'static', 'login.html')))
+
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
