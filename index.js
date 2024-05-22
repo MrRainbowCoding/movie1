39daefb4d0371b8ce0d6a01e9ca6cee2935cb11b
Mr. Rainbow <alexander.logsdon@stu.jefferson.kyschools.us>
2024-05-22 17:54:22 +0000

    e

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const path = require('path');
 
 const app = express();
 
+// DO NOT EDIT ANYTHING ABOVE UNLESS YOU KNOW WHAT UR DOING!
 
 app.use('/', createProxyMiddleware({
-    target: 'https://github.com/',
+    target: 'https://github.com/', // Enter the url to proxy here, or just make other paths by copying the box.
     changeOrigin: true,
 }));
 
-app.use('/signup', express.static(path.join(__dirname, 'static')))
-app.use('/login', express.static(path.join(__dirname, 'static')))
+// Disables sign up and login (uncomment to disable it)
 
+// app.use('/signup', express.static(path.join(__dirname, 'static')))
+// app.use('/login', express.static(path.join(__dirname, 'static')))
 
+// DO NOT EDIT BELOW THIS LINE.
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
